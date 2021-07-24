5fbb8a178cafae773fdcf603d04e8cda52622716
Rahul Sinha <rahulchampion48@gmail.com>
2021-07-24 22:40:23 +0530

    add menubar

---
+++ src/components/menubar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <div className="menubar">
+      <div className="menubar__left">
+        <div className="menubar--link">PORTFOLIO</div>
+      </div>
+      <div className="menubar__right">
+        <div className="menubar--link">About</div>
+        <div className="menubar--link">Project</div>
+        <div className="menubar--link">Expirience</div>
+        <div className="menubar--link">Achievements</div>
+        <div className="menubar--link">Contact Me</div>
+      </div>
+    </div>
+  );
+};
+
+export default navbar;
