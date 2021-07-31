b3226368df644fd6047e776558f2f436d16a0d06
Rahul Sinha <rahulchampion48@gmail.com>
2021-08-01 01:57:43 +0530

    add btn for going to the top, add smooth scrolling effect

--- src/components/menubar.js
+++ src/components/menubar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className="menubar">
+    <div id="top" className="menubar">
       <div className="menubar__left">
         <a href="" className="menubar--link">
           PORTFOLIO
@@ -52,6 +52,23 @@ const Navbar = () => {
           Contact Me
         </a>
       </div>
+      <a href="#top" className="toTop">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-arrow-up"
+        >
+          <line x1="12" y1="19" x2="12" y2="5"></line>
+          <polyline points="5 12 12 5 19 12"></polyline>
+        </svg>
+      </a>
     </div>
   );
 };
